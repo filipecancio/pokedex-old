dbb7c86fd91eb6c464d5dfe8ba4d91f6f009fca9
cancio <filipe.cancio@gmail.com>
2022-05-06 22:26:09 -0300

    :construction: New screens

--- app/src/main/java/dev/cancio/pokedex/ui/screen/SearchScreen.kt
+++ app/src/main/java/dev/cancio/pokedex/ui/screen/SearchScreen.kt
@@ -5,5 +5,5 @@ import androidx.compose.runtime.Composable
 
 @Composable
 fun SearchScreen(){
-    Text(text = "Liked Screen")
+    Text(text = "Search Screen")
 }
--- app/src/main/java/dev/cancio/pokedex/ui/screen/WhoScreen.kt
+++ app/src/main/java/dev/cancio/pokedex/ui/screen/WhoScreen.kt
@@ -5,5 +5,5 @@ import androidx.compose.runtime.Composable
 
 @Composable
 fun WhoScreen(){
-    Text(text = "Liked Screen")
+    Text(text = "Who Screen")
 }
