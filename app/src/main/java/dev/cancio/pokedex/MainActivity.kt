31055cc391bc856ecc8b61af691174861cd838d2
cancio <filipe.cancio@gmail.com>
2022-05-06 23:04:52 -0300

    :construction: Update Navigation

--- app/src/main/java/dev/cancio/pokedex/MainActivity.kt
+++ app/src/main/java/dev/cancio/pokedex/MainActivity.kt
@@ -8,37 +8,21 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.navigation.NavController
-import androidx.navigation.NavHostController
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import dev.cancio.pokedex.navigation.MainNavigation
+import dev.cancio.pokedex.navigation.BottomNavItem.*
 import dev.cancio.pokedex.theme.PokedexTheme
-import dev.cancio.pokedex.ui.component.BottomBar
-import dev.cancio.pokedex.ui.component.NavItem
-import dev.cancio.pokedex.ui.screen.HomeScreen
-import dev.cancio.pokedex.ui.screen.LikedScreen
-import dev.cancio.pokedex.ui.screen.SearchScreen
-import dev.cancio.pokedex.ui.screen.WhoScreen
+import dev.cancio.pokedex.component.BottomBar
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            PokedexTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    AppScreen()
-                }
-            }
+            AppScreen()
         }
     }
 }
@@ -46,20 +30,20 @@ class MainActivity : ComponentActivity() {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AppScreen() {
-    val navController = rememberNavController()
-    Scaffold(
-        bottomBar = { BottomBar(navController) }
-    ) {
-        Navigation(navController)
+    PokedexTheme {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colors.background
+        ) {
+            val navController = rememberNavController()
+            val itemList = listOf(Home, Search, Likes, Who)
+            Scaffold(
+                bottomBar = { BottomBar(navController, itemList) }
+            ) {
+                MainNavigation(navController, itemList )
+            }
+        }
     }
 }
 
-@Composable
-fun Navigation(navController: NavHostController) {
-    NavHost(navController = navController, startDestination = NavItem.Home.route){
-        composable(NavItem.Home.route) { HomeScreen()}
-        composable(NavItem.Likes.route) { LikedScreen() }
-        composable(NavItem.Search.route) { SearchScreen() }
-        composable(NavItem.Who.route) { WhoScreen() }
-    }
-}
+
